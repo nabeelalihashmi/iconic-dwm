733a21304cdbdef0a6af245dd75207f328a40c6d
Nabeel Ali <mail2nabeelali@gmail.com>
2022-03-31 09:50:02 +0500

    small changes

--- config.h
+++ config.h
@@ -10,13 +10,13 @@ static const unsigned int gappoh    = 4;       /* horiz outer gap between window
 static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 6;   /* systray spacing */
+static const unsigned int systrayonleft = 1;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "FontAwesome:size:10;Ubuntu:size=11" };
+static const char *fonts[]          = { "FontAwesome:size:11;Ubuntu:size=11" };
 static const char dmenufont[]       = "Ubuntu:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -120,6 +120,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_Print,  spawn,          {.v = scrshotselcmd } },
 	{ MODKEY|ShiftMask,				  XK_Print,  spawn,          {.v = scrshotwincmd } },
 	{ MODKEY|ShiftMask,			     XK_v,      spawn,          {.v = volctrlcmd } },
+	{ MODKEY,                       XK_p,  spawn,              {.v = dmenucmd } },
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
